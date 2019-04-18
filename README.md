797bc9ebc245d5b992fd32a293c8604d2365e273
姚康明 <154518484@qq.com>
2019-04-18 13:35:54 +0800

    init.

---
+++ README.md
@@ -0,0 +1,18 @@
+# kaptcha - A kaptcha generation engine.
+
+This repo is the copy of http://code.google.com/p/kaptcha/ and published to maven central
+```
+<dependency>
+  <groupId>com.github.penggle</groupId>
+  <artifactId>kaptcha</artifactId>
+  <version>2.3.2</version>
+</dependency>
+```
+Please see the website for more information about this project.
+
+http://code.google.com/p/kaptcha/
+
+thanks!
+
+
+compare with others, the jar has less dependencies, enjoy it.
---
+++ src/test/java/com/google/code/kaptcha/ProducerTest.java
@@ -0,0 +1,31 @@
+package com.google.code.kaptcha;
+
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.FileOutputStream;
+import java.util.Properties;
+
+public class ProducerTest {
+
+	public static void main(String[] args) throws Exception {
+
+		Properties properties = new Properties();
+		properties.setProperty("kaptcha.border", "yes");
+		properties.setProperty("kaptcha.border.color", "105,179,90");
+		properties.setProperty("kaptcha.textproducer.font.color", "blue");
+
+		Config config = new Config(properties);
+
+		Producer producer = new DefaultKaptcha();
+		((DefaultKaptcha) producer).setConfig(config);
+
+		BufferedImage bufferedImage = producer.createImage("ADSC6893");
+		ImageIO.write(bufferedImage, "jpg", new FileOutputStream("/Users/imiracle/Desktop/a.jpg"));
+
+
+	}
+
+}
